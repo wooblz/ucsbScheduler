bb75828bb6bf79af99b3b69861af970386c9aa98
wooblz <103292906+wooblz@users.noreply.github.com>
2026-01-06 21:31:49 -0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -2,12 +2,12 @@ package config
 
 import "time"
 
-    // Add logic for 20261 if needed, or other quarters
+    
     if quarter == "20261" {
         start := time.Date(2026, 1, 5, 0, 0, 0, 0, time.Local)
         end := time.Date(2026, 3, 20, 23, 59, 59, 0, time.Local)
         return start, end, nil
     }
-    // Return an error instead of time.Now()
+    
     return time.Time{}, time.Time{}, fmt.Errorf("quarter %s not defined", quarter)
 }
